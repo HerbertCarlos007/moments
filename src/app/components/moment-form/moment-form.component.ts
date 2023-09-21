ab55eb3985e35a787bb061cbcaca3b0a23c834b3
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-09-21 10:25:19 -0300

    pegando um momento individual pelo id

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { Moment } from 'src/app/interfaces/Moment';
 
---
+++ src/app/components/pages/moment/moment.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MomentService } from 'src/app/services/moment/moment.service';
+import { Moment } from 'src/app/interfaces/Moment';
+
+@Component({
+  selector: 'app-moment',
+  templateUrl: './moment.component.html',
+  styleUrls: ['./moment.component.css']
+})
+export class MomentComponent {
+  moment?: Moment
+
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.momentService.getMoment(id).subscribe(item => this.moment = item.data)
+  }
+
+}
