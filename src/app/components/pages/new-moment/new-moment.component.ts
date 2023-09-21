623ec37b6fa04340cc45ae6df500c958ac6bf2c2
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-09-20 23:52:49 -0300

    finalizando sistema de mensagens

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Moment } from 'src/app/interfaces/Moment'
-import {MomentService} from 'src/app/services/moment/moment.service'
+import { MomentService } from 'src/app/services/moment/moment.service'
+import { MessageService } from 'src/app/services/message/message.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -9,22 +11,30 @@ import {MomentService} from 'src/app/services/moment/moment.service'
 })
 export class NewMomentComponent {
   btnText = 'Compartilhar'
-  
-  constructor(private momentService: MomentService) {}
+
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessageService,
+    private router: Router
+  ) { }
 
   async createHandler(moment: Moment) {
     const formData = new FormData()
-    
+
     formData.append('title', moment.title)
     formData.append('description', moment.description)
-    
+
     if (moment.image) {
       formData.append('image', moment.image)
     }
-    
+
     await this.momentService.createMoment(formData).subscribe()
+
+    this.messagesService.add('Momento adicionado com sucesso!')
     
+    this.router.navigate(['/'])
+
   }
-  
-  
+
+
 }
